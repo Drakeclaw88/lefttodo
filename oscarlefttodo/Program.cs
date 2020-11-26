e6cbce9f4983666171958d94050e07cf0ce4384f
Oscar Åberg <oscar.aberg1@hotmail.com>
2020-11-27 00:28:01 +0100

    added list and welcome text

--- oscarlefttodo/Program.cs
+++ oscarlefttodo/Program.cs
@@ -6,7 +6,22 @@ namespace oscarlefttodo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var myTodo = new Todo();
+
+            System.Console.WriteLine("Welcome to oscars todo app");
+            System.Console.WriteLine("Here you can create task that you need to get done");
+            System.Console.WriteLine("You can list all of the task that created");
+            System.Console.WriteLine("You can mark the task as done and file them archive if you want");
+            System.Console.WriteLine("You can also see a list of all your archived tasks ");
+            System.Console.WriteLine("Easy and simple right?");
+            System.Console.WriteLine("If You understand then press any key to continue");
+            Console.ReadKey();
+            
+
+
+
+
         }
     }
 }
--- oscarlefttodo/Todo.cs
+++ oscarlefttodo/Todo.cs
@@ -1,11 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 namespace oscarlefttodo
 {
 
     class Todo
     {
+
+        List<Task> TaskList;
+        List<TaskDeadline> TaskDeadlineList;
+        List<TaskCheckList> TaskCheckList;
         
+        
+
+
+
+
+
         
     }
 
