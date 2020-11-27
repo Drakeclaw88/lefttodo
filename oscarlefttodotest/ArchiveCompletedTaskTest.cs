cc63cdd9db8bdd31b764e103af3e411dfd1ce987
Oscar Åberg <oscar.aberg1@hotmail.com>
2020-11-27 22:56:29 +0100

    finished unit test fpr archived tasks

--- oscarlefttodotest/ArchiveCompletedTaskTest.cs
+++ oscarlefttodotest/ArchiveCompletedTaskTest.cs
@@ -13,13 +13,47 @@ namespace oscarlefttodotest
         {
 
             //arrange
-    
+            List<Task> notCompletedTask = new List<Task>();
+            List<Task> completedAndArchivedList = new List<Task>();
+            Task one = new Task("", "");
+            Task two = new Task("", "");
+
+            notCompletedTask.Add(one);
+            notCompletedTask.Add(two);
+
+            one.Completed = !one.Completed;
+            two.Completed = !two.Completed;
+            var startingValue = completedAndArchivedList.Count;
+
+
+
+
+
             //act
             
+            foreach (var item in notCompletedTask)
+            {
+                if (item.Completed == true)
+                {
+                    item.Archived =true;
+                    
+                }
+                
+            }
+            foreach (var item in notCompletedTask)
+            {
 
+                if (item.Archived == true)
+                {
+                    completedAndArchivedList.Add(item);
+                }
+                
+            }
+            var endingValue = completedAndArchivedList.Count;
 
 
             //assert
+            Assert.NotEqual(startingValue,endingValue);
 
         }
     }
