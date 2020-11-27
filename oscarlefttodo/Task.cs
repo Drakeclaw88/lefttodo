ecc9a33b6460f6f1c49975311e16153c66b00250
Oscar Åberg <oscar.aberg1@hotmail.com>
2020-11-27 12:14:46 +0100

    same as prev commit

--- oscarlefttodo/Task.cs
+++ oscarlefttodo/Task.cs
@@ -5,7 +5,20 @@ namespace oscarlefttodo
 
     class Task
     {
-        
+
+
+        private string NameOfTask;
+        private string Description;
+        public bool Completed = false;
+
+        public string Description1 { get => Description; set => Description = value; }
+        public string NameOfTask1 { get => NameOfTask; set => NameOfTask = value; }
+
+        public Task(string nameOfTask, string description)
+        {
+            NameOfTask = nameOfTask;
+            Description = description;
+        }
     }
 
 
--- oscarlefttodo/Todo.cs
+++ oscarlefttodo/Todo.cs
@@ -6,106 +6,134 @@ namespace oscarlefttodo
 
     class Todo
     {
+
+
+        public List<Task> taskList = new List<Task>();
         public void TodoMeny()
         {
 
-             Console.WriteLine("Please choose one of these options");
-                Console.WriteLine("Write one of these numbers that correspond to your option");
 
-                int options = 0;
+
+
+
+            Console.WriteLine("Please choose one of these options");
+            Console.WriteLine("Write one of these numbers that correspond to your option");
+
+            int options = 0;
+            do
+            {
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("Choose what you want to do? Write the corresponding number too your choosen option");
+                Console.WriteLine("***********");
+                Console.WriteLine(" 1: List all tasks that you have created");
+                Console.WriteLine("***********");
+                Console.WriteLine(" 2: Create a new task");
+                Console.WriteLine("***********");
+                Console.WriteLine(" 3: Archive completed tasks");
+                Console.WriteLine("***********");
+                Console.WriteLine(" 4: List archived tasks");
+                Console.WriteLine("***********");
+                Console.WriteLine(" 0: Close the program");
+
+                // This loop of exceptions handling was really good to find out about, makes the program so much smoother when it runs,
+                // i used this for all exception handling in the code.
+                bool loop = true;
+
                 do
                 {
-                    Console.WriteLine("-----------------------------------------------");
-                    Console.WriteLine("Choose what you want to do? Write the corresponding number too your choosen option");
-                    Console.WriteLine("***********");
-                    Console.WriteLine(" 1: List all tasks that you have created");
-                    Console.WriteLine("***********");
-                    Console.WriteLine(" 2: Create a new task");
-                    Console.WriteLine("***********");
-                    Console.WriteLine(" 3: Archive completed tasks");
-                    Console.WriteLine("***********");
-                    Console.WriteLine(" 4: List archived tasks");
-                    Console.WriteLine("***********");
-                    Console.WriteLine(" 0: Close the program");
-
-                    // This loop of exceptions handling was really good to find out about, makes the program so much smoother when it runs,
-                    // i used this for all exception handling in the code.
-                    bool loop = true;
-
-                    do
+                    try
                     {
-                        try
-                        {
-                            options = Convert.ToInt32(Console.ReadLine());
-                            loop = false;
-                        }
-                        catch
-                        {
-                            Console.WriteLine(" Error: You wrote in the wrong format, please rechoose your option again. ");
-                        }
+                        options = Convert.ToInt32(Console.ReadLine());
+                        loop = false;
                     }
-                    while (loop);
-                    // the switch cases are linked to the user input, and the diffrent cases calls diffrent metods to be executed
-                    switch (options)
+                    catch
                     {
-                        case 0:
-                            Console.WriteLine("Simulation will be terminated!!");
-                            break;
-                        case 1:
-                            ListTasks();
-                            break;
-                        case 2:
-                            CreateTask();
-                            break;
-                        case 3:
-                            ArchiveCompletedTasks();
-                            break;
-                        case 4:
-                            ListArchivedTasks();
-                            break;
-                        default:
-                            WrongOption();
-                            break;
+                        Console.WriteLine(" Error: You wrote in the wrong format, please rechoose your option again. ");
                     }
-
                 }
-                while (options != 0);
+                while (loop);
+                // the switch cases are linked to the user input, and the diffrent cases calls diffrent metods to be executed
+                switch (options)
+                {
+                    case 0:
+                        Console.WriteLine("Simulation will be terminated!!");
+                        break;
+                    case 1:
+                        ListAllTasks();
+                        break;
+                    case 2:
+                        CreateNewTask();
+                        break;
+                    case 3:
+                        ArchiveCompletedTasks();
+                        break;
+                    case 4:
+                        ListAllArchivedTasks();
+                        break;
+                    default:
+                        WrongOption();
+                        break;
+                }
+
+            }
+            while (options != 0);
+        }
+
+        public void WrongOption()
+        {
+            System.Console.WriteLine("You must choose a number that correspond to an option. please choose 0-4");
+        }
+
+        public void ListAllArchivedTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ArchiveCompletedTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateNewTask()
+        {
+            string taskName;
+            string descriptionOfTask;
+
+            System.Console.WriteLine("What name do you want to give your task? ");
+            taskName = Console.ReadLine();
+            System.Console.WriteLine("Write a short description of your task");
+            descriptionOfTask = Console.ReadLine();
+
+            taskList.Add(new Task(taskName,descriptionOfTask));
+
+
+        }
+
+        public void ListAllTasks()
+        {
+
+            
+
+            foreach (var item in taskList)
+            {
+
+                System.Console.WriteLine(item.NameOfTask1);
+                System.Console.WriteLine(item.Description1);
+                System.Console.WriteLine(item.Description1);
+
             }
 
-        private void WrongOption()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ListArchivedTasks()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ArchiveCompletedTasks()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CreateTask()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ListTasks()
-        {
-            throw new NotImplementedException();
         }
     }
-        
-        
 
 
 
 
 
-        
-    }
+
+
+
+}
 
 
 
