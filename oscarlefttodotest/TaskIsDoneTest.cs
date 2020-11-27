e97b023c2830fa991663cb2f4ba7d3a571d68b85
Oscar Åberg <oscar.aberg1@hotmail.com>
2020-11-27 22:06:37 +0100

    done unit test to check if task is done

--- oscarlefttodotest/TaskIsDoneTest.cs
+++ oscarlefttodotest/TaskIsDoneTest.cs
@@ -13,13 +13,19 @@ namespace oscarlefttodotest
         {
 
             //arrange
+            Task testTaskOne = new Task("", "");
+            System.Console.WriteLine(testTaskOne.Completed);
+            var expected = testTaskOne.Completed;
 
 
             //act
+            testTaskOne.Completed = !testTaskOne.Completed;
+            var actual = testTaskOne.Completed;
             
 
 
             //assert
+            Assert.NotEqual(expected,actual);
 
         }
     }
